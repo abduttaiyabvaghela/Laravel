10afd68ada3812a4a391978d0be606e41cb1d2cd
Taiyeb Vaghela <respect.babji93@gmail.com>
2020-04-24 20:06:58 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('post', 'PostsController');
+
+Route::get('about', function () {
+    return '<h1>About Page</h1>';
+});
+
+Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'This is Comments ID : '.$commentId; 
+});
+
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
+});
